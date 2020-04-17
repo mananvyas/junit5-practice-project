57b3e79ec544b0fb6c5c2b9f488095f49b23946c
Manan <mananvyas@gmail.com>
2020-04-17 22:49:13 +0530

    Simple Tests using JUnit5

---
+++ src/main/java/general/MathUtils.java
@@ -0,0 +1,15 @@
+package general;
+
+public class MathUtils {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+  /*public static void main(String[] args) {
+
+    }*/
+}
---
+++ src/test/java/general/MathUtilsTest.java
@@ -0,0 +1,57 @@
+package general;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    MathUtils mathUtils = new MathUtils();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addPositiveInteger() {
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addOneNegativeInteger() {
+        int expected = 3;
+        int actual = mathUtils.add(5,-2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void addNegativeInteger() {
+        int expected = -6;
+        int actual = mathUtils.add(-4,-2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDivide(){
+        //AssertThrows  - Very useful assertion
+
+        //Important Note: It takes 2 arguments -
+        // first arg: expectedType which is nothing but expected exception from the specific method call
+        // second arg: executable - which is nothing but a function which might generate an exception
+        //Other way around is to use try catch but this is compact version using lambdas.
+
+        //Note: Might need to set language level from IntelliJ Project Structure -> Module -> select Java 8 or more
+
+        //Comment and Uncomment following 2 lines to verify assertThrows working
+        assertThrows(ArithmeticException.class, () ->  mathUtils.divide(1, 0));
+        //assertThrows(NullPointerException.class, () ->  mathUtils.divide(1, 0));
+    }
+}
